f6ff8903de543a40f4908fe97dfb272c027f39c3
Omnia Arafat <83679206+Omnia-Arafat@users.noreply.github.com>
2023-08-15 04:21:31 +0200

    slider almost done
    there are some issues with linking the origin images, I don't know why they don't appear I hope that is a problem in my browser so I will make another teammate try it, then I will put replace  the text

--- flexa/components/HomeOne/MainBanner.js
+++ flexa/components/HomeOne/MainBanner.js
@@ -1,84 +1,220 @@
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Link from "next/link";
 
 const MainBanner = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+
   return (
     <>
       <div className="main-banner-area">
-        <div className="container-fluid">
-          <div className="row align-items-center">
-            <div className="col-lg-6">
-              <div className="banner-text">
-                <span
-                  data-aos="fade-up"
-                  data-aos-delay="200"
-                  data-aos-duration="1200"
-                  data-aos-once="true"
-                >
-                  Investment Retirement Insurance
-                </span>
+        <Slider {...settings}>
+          <div className="banner-slide">
+            {/* Slide 1 */}
+            <div className="container-fluid">
+            <div className="row align-items-center">
+                <div className="col-lg-6">
+                  <div className="banner-text">
+                    <span
+                      data-aos="fade-up"
+                      data-aos-delay="200"
+                      data-aos-duration="1200"
+                      data-aos-once="true"
+                    >
+                      Slide 1 Title
+                    </span>
 
-                <h1
-                  data-aos="fade-up"
-                  data-aos-delay="300"
-                  data-aos-duration="1200"
-                  data-aos-once="true"
-                >
-                  The Right Protection to Keep You Moving Forward
-                </h1>
+                    <h1
+                      data-aos="fade-up"
+                      data-aos-delay="300"
+                      data-aos-duration="1200"
+                      data-aos-once="true"
+                    >
+                      Slide 1 Content
+                    </h1>
 
-                <div
-                  data-aos="fade-up"
-                  data-aos-delay="400"
-                  data-aos-duration="1200"
-                  data-aos-once="true"
-                >
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Quis ipsum.
-                  </p>
+                    <div
+                      data-aos="fade-up"
+                      data-aos-delay="400"
+                      data-aos-duration="1200"
+                      data-aos-once="true"
+                    >
+                      <p>Slide 1 Description</p>
+                    </div>
+
+                    <div
+                      className="banner-btn"
+                      data-aos="fade-up"
+                      data-aos-delay="500"
+                      data-aos-duration="1200"
+                      data-aos-once="true"
+                    >
+                      <Link href="/contact">
+                        <a className="default-btn">Get Started</a>
+                      </Link>
+
+                      <Link href="/contact">
+                        <a className="default-btn active">Find An Agent</a>
+                      </Link>
+                    </div>
+                  </div>
                 </div>
 
-                <div
-                  className="banner-btn"
-                  data-aos="fade-up"
-                  data-aos-delay="500"
-                  data-aos-duration="1200"
-                  data-aos-once="true"
-                >
-                  <Link href="/contact">
-                    <a className="default-btn">Get Started</a>
-                  </Link>
-
-                  <Link href="/contact">
-                    <a className="default-btn active">Find An Agent</a>
-                  </Link>
+                <div className="col-lg-6 pr-0">
+                  <div
+                    className="banner-img"
+                    data-aos="fade-up"
+                    data-aos-delay="600"
+                    data-aos-duration="1200"
+                    data-aos-once="true"
+                  >
+                    <img src="https://picsum.photos/id/25/5000/3333" alt="Image" />
+                  </div>
                 </div>
-              </div>
-            </div>
+              </div>            </div>
+          </div>
 
-            <div className="col-lg-6 pr-0">
-              <div
-                className="banner-img"
-                data-aos="fade-up"
-                data-aos-delay="600"
-                data-aos-duration="1200"
-                data-aos-once="true"
-              >
-                <img src="/images/banner/banner-img.png" alt="Image" />
+          <div className="banner-slide">
+            {/* Slide 2 */}
+            <div className="container-fluid">
+            <div className="row align-items-center">
+                <div className="col-lg-6">
+                  <div className="banner-text">
+                    <span
+                      data-aos="fade-up"
+                      data-aos-delay="200"
+                      data-aos-duration="1200"
+                      data-aos-once="true"
+                    >
+                      Slide 2 Title
+                    </span>
+
+                    <h1
+                      data-aos="fade-up"
+                      data-aos-delay="300"
+                      data-aos-duration="1200"
+                      data-aos-once="true"
+                    >
+                      Slide 2 Content
+                    </h1>
+
+                    <div
+                      data-aos="fade-up"
+                      data-aos-delay="400"
+                      data-aos-duration="1200"
+                      data-aos-once="true"
+                    >
+                      <p>Slide 2 Description</p>
+                    </div>
+
+                    <div
+                      className="banner-btn"
+                      data-aos="fade-up"
+                      data-aos-delay="500"
+                      data-aos-duration="1200"
+                      data-aos-once="true"
+                    >
+                      <Link href="/contact">
+                        <a className="default-btn">Get Started</a>
+                      </Link>
+
+                      <Link href="/contact">
+                        <a className="default-btn active">Find An Agent</a>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-lg-6 pr-0">
+                  <div
+                    className="banner-img"
+                    data-aos="fade-up"
+                    data-aos-delay="600"
+                    data-aos-duration="1200"
+                    data-aos-once="true"
+                  >
+                    <img src="https://picsum.photos/id/25/5000/3333" alt="Image" />
+                  </div>
+                </div>
+              </div>            </div>
+          </div>
+
+          <div className="banner-slide">
+            {/* Slide 3 */}
+            <div className="container-fluid">
+              <div className="row align-items-center">
+                <div className="col-lg-6">
+                  <div className="banner-text">
+                    <span
+                      data-aos="fade-up"
+                      data-aos-delay="200"
+                      data-aos-duration="1200"
+                      data-aos-once="true"
+                    >
+                      Slide 3 Title
+                    </span>
+
+                    <h1
+                      data-aos="fade-up"
+                      data-aos-delay="300"
+                      data-aos-duration="1200"
+                      data-aos-once="true"
+                    >
+                      Slide 3 Content
+                    </h1>
+
+                    <div
+                      data-aos="fade-up"
+                      data-aos-delay="400"
+                      data-aos-duration="1200"
+                      data-aos-once="true"
+                    >
+                      <p>Slide 3 Description</p>
+                    </div>
+
+                    <div
+                      className="banner-btn"
+                      data-aos="fade-up"
+                      data-aos-delay="500"
+                      data-aos-duration="1200"
+                      data-aos-once="true"
+                    >
+                      <Link href="/contact">
+                        <a className="default-btn">Get Started</a>
+                      </Link>
+
+                      <Link href="/contact">
+                        <a className="default-btn active">Find An Agent</a>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-lg-6 pr-0">
+                  <div
+                    className="banner-img"
+                    data-aos="fade-up"
+                    data-aos-delay="600"
+                    data-aos-duration="1200"
+                    data-aos-once="true"
+                  >
+                    <img src="https://picsum.photos/id/25/5000/3333" alt="Image" />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-
-        {/* Shape Images */}
-        <div className="shape">
-          <img src="/images/banner/banner-shape.png" alt="Image" />
-        </div>
-        <div className="banner-shape-right">
-          <img src="/images/banner/banner-shape-right.png" alt="Image" />
-        </div>
+        </Slider>
       </div>
     </>
   );
