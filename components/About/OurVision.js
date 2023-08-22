5082ebe5d7297670b9e415c067241044b69e190b
Omnia Arafat <83679206+Omnia-Arafat@users.noreply.github.com>
2023-08-22 15:41:45 +0200

    landing page is done
    fonts updated and images gaps removed

--- components/About/OurVision.js
+++ components/About/OurVision.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const OurVision = () => {
-    return (<>
-    <div className='section-title'>
-      <h3 className='span' style={{textAlign:'center',marginBottom:'90px',color:'#F0702F',backgroundColor:'transparent',fontSize:"57px"}}> How it works ?</h3>
-			</div>
-      <div>
+  return (
+    <>
+      <div className='span section-title'>
+        <span style={{ textAlign: 'center', color: '#F0702F', backgroundColor: 'transparent' }}> How it works ?</span>
+        <h2 style={{ marginTop: '10px' }}>Beyond Insurance <br />We are here for you</h2>
+      </div>
 
-        <img src="/images/works1.png" alt="logo" style={{width:'50%'}}/>
-        <img src="/images/works2.png" alt="logo" style={{width:'50%'}}/>
-
-
-		</div></>
-    )
+      <div className='img-cont'>
+        <Container>
+          <Row style={{ margin: 0, padding: 0 }}>
+            <Col style={{ paddingRight: 0, paddingLeft: 0 }}>
+              <img src="/images/works1.png" alt="logo" style={{ width: '100%' }} />
+            </Col>
+            <Col style={{ paddingRight: 0, paddingLeft: 0 }}>
+              <img src="/images/works2.png" alt="logo" style={{ width: '100%' }} />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
 }
 
 export default OurVision;
