20d0a65b5274cba992d5becea65865700972b4f9
Omnia Arafat <83679206+Omnia-Arafat@users.noreply.github.com>
2023-08-23 12:36:45 +0200

    dummy commit

--- build/static/chunks/webpack.js
+++ build/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fdf9d3131db6088b"; }
+/******/ 		__webpack_require__.h = function() { return "3ff61ba5e25d18ca"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
