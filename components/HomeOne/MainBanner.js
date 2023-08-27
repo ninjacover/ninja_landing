dd304998bd3f6c7d13276bce292177b44a36f213
Omnia Arafat <83679206+Omnia-Arafat@users.noreply.github.com>
2023-08-27 16:06:18 +0200

    dummy commit

--- components/HomeOne/MainBanner.js
+++ components/HomeOne/MainBanner.js
@@ -1,3 +1,4 @@
+
 // MainBanner.js
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
@@ -12,9 +13,9 @@ const MainBanner = () => {
 
   useEffect(() => {
     const newSlides = [
-      { url: "/images/banner/first-slider-img-res.png", title: "chatbot" },
-      { url: "/images/banner/sec-slider-img-res.png", title: "boat" },
-      { url: "/images/banner/3rd-slider-img-res.png", title: "forest" },
+      { url: "/images/first-slider-img-res", title: "chatbot" },
+      { url: "/images/sec-slider-img-res", title: "boat" },
+      { url: "/images/3rd-slider-img-res", title: "forest" },
     ];
 
     const mediaQuery = window.matchMedia("(max-width:780px)");
