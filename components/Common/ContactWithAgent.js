fd86c6145a9bac3f7ea8cbab6d404ca5a484a0bc
Ahmed Wael <a.wael@hadbrok.com>
2023-09-11 15:19:27 +0300

    Clearing branch main

--- components/Common/ContactWithAgent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ContactWithAgent = () => {
-    return (
-      <></>
-    )
-}
-
-export default ContactWithAgent;
--- components/Common/FunFactsStyleTwo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const FunFactsStyleTwo = () => {
-    return (
-  <></>
-    )
-}
-
-export default FunFactsStyleTwo;
