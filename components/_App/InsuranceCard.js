e6ad7da5849844b95beb838867e1c075e16b9c25
Omnia Arafat <83679206+Omnia-Arafat@users.noreply.github.com>
2023-09-13 02:00:02 +0300

    popup form added and it's responsive

--- components/_App/InsuranceCard.js
+++ components/_App/InsuranceCard.js
@@ -1,6 +1,12 @@
 import React, { useState,useEffect } from 'react';
 import SwiperCore, { Navigation } from 'swiper/core';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import InsuranceTable from './InsuranceTable';
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+
 import {
   FaUserMd,
   FaHospital,
@@ -12,6 +18,7 @@ import {
   FaBabyCarriage,
   FaPills,
 } from 'react-icons/fa';
+// import { Link } from 'react-router-dom';
 
 SwiperCore.use([Navigation]);
 
@@ -55,6 +62,16 @@ const InsuranceCard = ({ data }) => {
     setIsExpanded(!isExpanded);
   };
 
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const openPopup = () => {
+    setIsPopupOpen(true);
+    console.log(setIsPopupOpen);
+  };
+
+  const closePopup = () => {
+    setIsPopupOpen(false);
+  };
+
   return (
     <div className={`insurance-card-container ${isExpanded ? 'expanded' : ''}`}>
 
@@ -67,7 +84,7 @@ const InsuranceCard = ({ data }) => {
           <div className=""> {data.groupPremium} EGP</div>
         </div>
         <div className="cell">
-          <button className="select-button-responsive">Select</button>
+          <button className="select-button-responsive" onClick={openPopup.bind(this)}>Select</button>
         </div>
       </div>
       <div className="swiper-container">
@@ -109,6 +126,83 @@ const InsuranceCard = ({ data }) => {
       
 
     </div>
+
+    {isPopupOpen && (
+        <div className="popup overlay">
+          <div className="popup-content">
+            {/* Your form content goes here */}
+
+            <div className="form-heading text-center">
+              <h3 className="form-title">Log In to your account!</h3>
+              <p className="form-desc">With your social network.</p>
+            </div>
+
+
+            <form method="post">
+              <div className="row">
+              <div className="col-lg-4 col-md-4 col-sm-12" style={{ width: '0%', paddingRight: '1px', marginRight: '-1rem' }}>
+                  <button className="default-btn" type="submit">
+                    Google
+                  </button>
+                </div>
+                <div className="col-lg-4 col-md-4 col-sm-12" style={{width: '0%',paddingLeft: '0',marginRight: '-1rem'}}>
+                  <button className="default-btn" type="submit">
+                    Facebook
+                  </button>
+                </div>
+                <div className="col-lg-4 col-md-4 col-sm-12" style={{width: '0',paddingLeft: '0',marginRight: '5rem'}}>
+                  <button className="default-btn" type="submit">
+                    Twitter
+                  </button>
+                </div>
+
+                <div className="col-12">
+                  <div className="form-group">
+                    <input className="form-control" type="text" name="name" placeholder="Username or Email" />
+                  </div>
+                </div>
+                <div className="col-12">
+                  <div className="form-group">
+                    <input className="form-control" type="password" name="password" placeholder="Password" />
+                  </div>
+                </div>
+
+                <div className="col-lg-6 col-sm-6 form-condition">
+                  <div className="agree-label">
+                    <input type="checkbox" id="chb1" />
+                    <label htmlFor="chb1">Remember Me</label>
+                  </div>
+                </div>
+
+                <div className="col-lg-6 col-sm-6">
+                  <Link href="/recover-password">
+                    <a className="forget">Forgot my password?</a>
+                  </Link>
+                </div>
+
+                <div className="col-12">
+                  <button className="default-btn btn-two" type="submit">
+                    Log In Now
+                  </button>
+                </div>
+
+                <div className="col-12">
+                  <p className="account-desc">
+                    Not a member?
+                    <Link href="/sign-up"><a>Sign Up</a></Link>
+                  </p>
+                </div>
+              </div>
+            </form>
+
+            <div className='popup-close-icon'>
+
+
+              <FontAwesomeIcon icon={faTimes} onClick={closePopup} />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
