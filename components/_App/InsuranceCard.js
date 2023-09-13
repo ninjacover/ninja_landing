02ff27ed3ea6a9b3ea9ffa471d888aa27d8e611e
Ahmed Wael <135510135+awaell@users.noreply.github.com>
2023-09-13 14:56:14 +0300

    Merged branch 'dev' into 'master' (#32)
    * implemented the logic
    
    * insurance card css done
    
    * Added the landing page quotation form CSS and modified some fields in certain tabs to align with backend integration.
    
    * popup form added and it's responsive
    
    * Re-added insurance card styles for mobile responsive
    
    * Adding some styles for the quotation form responsive
    
    ---------
    
    Co-authored-by: Elkott1 <103424889+Elkott1@users.noreply.github.com>
    Co-authored-by: Omnia Arafat <83679206+Omnia-Arafat@users.noreply.github.com>

--- components/_App/InsuranceCard.js
+++ components/_App/InsuranceCard.js
@@ -1,6 +1,13 @@
 import React, { useState,useEffect } from 'react';
 import SwiperCore, { Navigation } from 'swiper/core';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import InsuranceTable from './InsuranceTable';
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+
+
 import {
   FaUserMd,
   FaHospital,
@@ -17,76 +24,143 @@ SwiperCore.use([Navigation]);
 
 
 const InsuranceCard = ({ data }) => {
-  const [swiper, setSwiper] = useState(null);
-  const [showScrollHint, setShowScrollHint] = useState(true);
+
   const [isExpanded, setIsExpanded] = useState(false); // State to track expansion
 
-
-  useEffect(() => {
-    // Hide the scroll hint after 1 second
-    const timeoutId = setTimeout(() => {
-      setShowScrollHint(false);
-    }, 1000);
-  
-    // Clear the timeout when the component unmounts
-    return () => clearTimeout(timeoutId);
-  }, []);
-
-  const icons = [
-    <FaHospital className="card-icon" />,
-    <FaUserMd className="card-icon" />,
-    <FaPills className="card-icon" />,
-    <FaCapsules className="card-icon" />,
-    <FaTooth className="card-icon" />,
-    <FaEye className="card-icon" />,
-    <FaBabyCarriage className="card-icon" />,
-  ];
-
-  const handleSwipe = (direction) => {
-    if (swiper) {
-      if (direction === 'left') {
-        swiper.slidePrev();
-      } else if (direction === 'right') {
-        swiper.slideNext();
-      }
-    }
-  };
   const toggleExpansion = () => {
     setIsExpanded(!isExpanded);
+  };
+  const dummyUser = {
+    name: 'John Doe',
+    averageAge: 32,
+    company: 'ABC Insurance',
+    employees: 100,
+  };
+
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const openPopup = () => {
+    setIsPopupOpen(true);
+    console.log(setIsPopupOpen);
+  };
+
+  const closePopup = () => {
+    setIsPopupOpen(false);
   };
 
   return (
     <div className={`insurance-card-container ${isExpanded ? 'expanded' : ''}`}>
 
     <div className="insurance-card">
-    <div className="row">
-        <div className="cell">
-          <div className="plan-header"> {data.plan}</div>
+    <div className="card-row">
+        <div className="card-cell">
+        <img src='/images/metlife2.png' className='company-image'></img>
+        </div>
+        <div className="card-cell">
+          <div className="card-price"> {data.groupPremium} </div>
+          <div className='card-small-text'>Avg/Prerson680</div>
         </div>
         <div className="cell">
-          <div className=""> {data.groupPremium} EGP</div>
-        </div>
-        <div className="cell">
-          <button className="select-button-responsive">Select</button>
+          <button className="select-button-responsive" onClick={openPopup.bind(this)}>Select</button>
+
         </div>
       </div>
-      <div className="swiper-container">
-      {/* <div className={`show-scroll-hint ${showScrollHint ? 'show-scroll-hint' : ''}`}>&#x1F449; Scroll</div> */}
-        <Swiper
-          onSwiper={(swiper) => setSwiper(swiper)}
-          slidesPerView={3} // Number of icons to show at a time
-          spaceBetween={10} // Space between icons
-          navigation={{
-            nextEl: '.icon-scroll-right',
-            prevEl: '.icon-scroll-left',
-          }}
-        >
-          {icons.map((icon, index) => (
-            <SwiperSlide key={index} className="icon">
-              {icon}
-            </SwiperSlide>
-          ))}
-        </Swiper>
+      <div >
+    
+    <div className="card-sectors">
+    
+    {/* Sector for In Patient */}
+    <div className="sector">
+      <div className="sector-header">In Patient</div>
+      <div className="sector-icons">
+        <FaHospital className='card-icon'/>
+        {data.inPatient ? <FaCheckCircle className='card-check'/> : <FaTimesCircle className='card-cross' />}
+      </div>
+      <div className="sector-footer">
+        {data.inPatient ? `${data.inPatientCoverage}` : 'Not Covered'}
+      </div>
+    </div>
+
+    {/* Sector for Out Patient */}
+    
+    <div className="sector">
+      <div className="sector-header">Out Patient</div>
+      <div className="sector-icons">
+        <FaUserMd className='card-icon' />
+        {data.outPatient ? <FaCheckCircle className='card-check' /> : <FaTimesCircle className='card-cross'/>}
+      </div>
+      <div className="sector-footer">
+        {data.outPatient ? 'Covered' : 'Not Covered'}
+      </div>
+    </div>
+
+    {/* Sector for Diseases */}
+   
+    <div className="sector">
+      <div className="sector-header">Diseases</div>
+      <div className="sector-icons">
+        <FaPills className='card-icon' />
+        {data.chronicPreExistingDisease ? <FaCheckCircle className='card-check'/> : <FaTimesCircle className='card-cross' />}
+      </div>
+      <div className="sector-footer">
+        {data.chronicPreExistingDisease ? 'Covered' : 'Not Covered'}
+      </div>
+    </div>
+
+    {/* Sector for Medication */}
+
+    <div className="sector">
+      <div className="sector-header">Medication</div>
+      <div className="sector-icons">
+        <FaCapsules className='card-icon' />
+        {data.medication ? <FaCheckCircle className='card-check'/> : <FaTimesCircle className='card-cross' />}
+      </div>
+      <div className="sector-footer">
+        {data.medication ? 'Covered' : 'Not Covered'}
+      </div>
+    </div>
+
+    {/* Sector for dental */}
+
+    <div className="sector">
+      <div className="sector-header">Dental</div>
+      <div className="sector-icons">
+        <FaTooth className='card-icon' />
+        {data.dental ? <FaCheckCircle className='card-check'/> : <FaTimesCircle className='card-cross' />}
+      </div>
+      <div className="sector-footer">
+        {data.dental ? 'Covered' : 'Not Covered'}
+      </div>
+    </div>
+
+    {/* Sector for Optical */}
+
+    <div className="sector">
+      <div className="sector-header">Optical</div>
+      <div className="sector-icons">
+        <FaEye className='card-icon' />
+        {data.optical ? <FaCheckCircle className='card-check' /> : <FaTimesCircle className='card-cross'/>}
+      </div>
+      <div className="sector-footer">
+        {data.optical ? 'Covered' : 'Not Covered'}
+      </div>
+    </div>
+
+    {/* Sector for Maternity */}
+
+    <div className="sector">
+      <div className="sector-header">Maternity</div>
+      <div className="sector-icons">
+        <FaBabyCarriage className='card-icon'/>
+        {data.maternity ? <FaCheckCircle className='card-check' /> : <FaTimesCircle className='card-cross' />}
+      </div>
+      <div className="sector-footer">
+        {data.maternity ? 'Covered' : 'Not Covered'}
+      </div>
+    </div>
+  </div>
+
+
+
       </div>
       <div className={`expand-link`} onClick={toggleExpansion}>
       {isExpanded ? 'Less Details' : 'More Details'}
@@ -105,10 +179,84 @@ const InsuranceCard = ({ data }) => {
       )}
 
       </div>
-
-      
-
     </div>
+
+    {isPopupOpen && (
+        <div className="popup overlay">
+          <div className="popup-content">
+            {/* Your form content goes here */}
+
+            <div className="form-heading text-center">
+              <h3 className="form-title">Log In to your account!</h3>
+              <p className="form-desc">With your social network.</p>
+            </div>
+
+
+            <form method="post">
+              <div className="row">
+              <div className="col-lg-4 col-md-4 col-sm-12" style={{ width: '0%', paddingRight: '1px', marginRight: '-1rem' }}>
+                  <button className="default-btn" type="submit">
+                    Google
+                  </button>
+                </div>
+                <div className="col-lg-4 col-md-4 col-sm-12" style={{width: '0%',paddingLeft: '0',marginRight: '-1rem'}}>
+                  <button className="default-btn" type="submit">
+                    Facebook
+                  </button>
+                </div>
+                <div className="col-lg-4 col-md-4 col-sm-12" style={{width: '0',paddingLeft: '0',marginRight: '5rem'}}>
+                  <button className="default-btn" type="submit">
+                    Twitter
+                  </button>
+                </div>
+
+                <div className="col-12">
+                  <div className="form-group">
+                    <input className="form-control" type="text" name="name" placeholder="Username or Email" />
+                  </div>
+                </div>
+                <div className="col-12">
+                  <div className="form-group">
+                    <input className="form-control" type="password" name="password" placeholder="Password" />
+                  </div>
+                </div>
+
+                <div className="col-lg-6 col-sm-6 form-condition">
+                  <div className="agree-label">
+                    <input type="checkbox" id="chb1" />
+                    <label htmlFor="chb1">Remember Me</label>
+                  </div>
+                </div>
+
+                <div className="col-lg-6 col-sm-6">
+                  <Link href="/recover-password">
+                    <a className="forget">Forgot my password?</a>
+                  </Link>
+                </div>
+
+                <div className="col-12">
+                  <button className="default-btn btn-two" type="submit">
+                    Log In Now
+                  </button>
+                </div>
+
+                <div className="col-12">
+                  <p className="account-desc">
+                    Not a member?
+                    <Link href="/sign-up"><a>Sign Up</a></Link>
+                  </p>
+                </div>
+              </div>
+            </form>
+
+            <div className='popup-close-icon'>
+
+
+              <FontAwesomeIcon icon={faTimes} onClick={closePopup} />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
